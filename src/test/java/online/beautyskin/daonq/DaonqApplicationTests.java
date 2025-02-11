4f7c3efd06fba105e8a7790e8f78049709ad7420
Nguyen Quang Dao <nguyenquangdao1408@gmail.com>
2025-02-11 10:04:43 +0700

    first commit

---
+++ src/test/java/online/beautyskin/daonq/DaonqApplicationTests.java
@@ -0,0 +1,13 @@
+package online.beautyskin.daonq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaonqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

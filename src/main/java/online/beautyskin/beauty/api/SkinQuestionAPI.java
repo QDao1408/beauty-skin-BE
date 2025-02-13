a018004f6dcea0f4840b74c0475762a008b3dd2e
datctse181971 <datctse181971@fpt.edu.vn>
2025-02-13 15:38:54 +0700

    add entity repository service api of skin question

---
+++ src/main/java/online/beautyskin/beauty/api/SkinQuestionAPI.java
@@ -0,0 +1,44 @@
+package online.beautyskin.beauty.api;
+
+import online.beautyskin.beauty.entity.SkinQuestion;
+import online.beautyskin.beauty.service.SkinQuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/skinQuestion")
+public class SkinQuestionAPI {
+    List<SkinQuestion> skinQuestionList = new ArrayList<>();
+    @Autowired
+    SkinQuestionService skinQuestionService;
+
+    @PostMapping("create")
+    public ResponseEntity createSkinQuestion(@RequestBody SkinQuestion skinQuestion){
+        SkinQuestion newSkinQuest = skinQuestionService.createSkinQuestion(skinQuestion);
+        return ResponseEntity.ok(newSkinQuest);
+    }
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity deleteSkinQuestion(@PathVariable long id){
+        SkinQuestion skinQuestion = skinQuestionService.removeSkinQuestion(id);
+        return ResponseEntity.ok(skinQuestion);
+    }
+    @PutMapping("update/{id}")
+    public ResponseEntity updateSkinQuestion(@PathVariable long id,@RequestBody SkinQuestion skinQuestion){
+        SkinQuestion newSkinQuestion = skinQuestionService.updateSkinQuestion(skinQuestion);
+        return ResponseEntity.ok(newSkinQuestion);
+    }
+    @GetMapping("getAll/{id}")
+    public ResponseEntity getAllQuestion(){
+        skinQuestionList = skinQuestionService.getSkinQuestion();
+        return ResponseEntity.ok(skinQuestionList);
+    }
+    @GetMapping("getDeleteIsFalse")
+    public ResponseEntity getDeleteIsFalse(){
+        skinQuestionList = skinQuestionService.getSkinQuestionDeleteIsFalse();
+        return ResponseEntity.ok(skinQuestionList);
+    }
+}

5dcdd058597ab0bd67d90a765f5f305ea0ce3554
Nguyen Quang Dao <nguyenquangdao1408@gmail.com>
2025-02-14 22:13:30 +0700

    fix AuthenticateRepository, AuthenticateService, PromotionRepository

--- src/main/java/online/beautyskin/beauty/repository/PromotionRepository.java
+++ src/main/java/online/beautyskin/beauty/repository/PromotionRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDate;
 
 public interface PromotionRepository extends JpaRepository<Promotion, Long> {
-    Promotion createPromotion(LocalDate startDate, LocalDate endDate);
-    Promotion findPromotionByDate(LocalDate date);
-    Promotion findPromotionBetween(LocalDate startDate, LocalDate endDate);
+
     
 }

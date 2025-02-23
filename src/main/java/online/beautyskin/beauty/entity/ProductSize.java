47928e47992cbfa3309dbf8e5ce4a8e327f93738
Nguyen Quang Dao <nguyenquangdao1408@gmail.com>
2025-02-23 19:37:18 +0700

    edit  product size

--- src/main/java/online/beautyskin/beauty/entity/ProductSize.java
+++ src/main/java/online/beautyskin/beauty/entity/ProductSize.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 
 @Entity
-public class Volume {
+public class ProductSize {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "Id")
@@ -12,13 +12,23 @@ public class Volume {
     @Column(name = "Volume", nullable = false)
     @Min(0)
     private int volume;
+    @Column(name = "Weight", nullable = false)
+    @Min(0)
+    private int weight;
+    @Column(name = "Price", nullable = false)
+    @Min(0)
+    private double price;
     private boolean isDeleted = false;
 
-    public Volume() {}
-    public Volume(int volume) { this.volume = volume; }
+    public ProductSize() {}
+    public ProductSize(int volume, int weight, double price) { this.volume = volume; this.weight = weight; this.price = price; }
     public long getId() { return id; }
     public int getVolume() { return volume; }
     public void setVolume(int volume) { this.volume = volume; }
     public boolean isDeleted() { return isDeleted; }
     public void setDeleted(boolean isDeleted) { this.isDeleted = isDeleted; }
+    public int getWeight() { return weight; }
+    public void setWeight(int weight) { this.weight = weight; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
 }

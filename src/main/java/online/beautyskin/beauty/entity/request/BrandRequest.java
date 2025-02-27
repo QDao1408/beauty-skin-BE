9609228b452e3e25d6387b036db51b011e550691
Nguyen Quang Dao <nguyenquangdao1408@gmail.com>
2025-02-27 13:51:21 +0700

    edit brand service, request

--- src/main/java/online/beautyskin/beauty/entity/request/BrandRequest.java
+++ src/main/java/online/beautyskin/beauty/entity/request/BrandRequest.java
@@ -2,7 +2,11 @@ package online.beautyskin.beauty.entity.request;
 
 public class BrandRequest {
     private String name;
+    private String description;
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
 
 }

73fdad89676a0105840bb5cc128ab1ea0ebbeb3a
Nguyen Quang Dao <nguyenquangdao1408@gmail.com>
2025-02-28 18:32:56 +0700

    CRD user address

---
+++ src/main/java/online/beautyskin/beauty/entity/request/UserAddressRequest.java
@@ -0,0 +1,69 @@
+package online.beautyskin.beauty.entity.request;
+
+public class UserAddressRequest {
+    private long userId;
+    private String receiverName;
+    private String receiverPhone;
+    private String receiverAddress;
+    private String city;
+    private String district;
+    private String ward;
+
+    public UserAddressRequest() {}
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public String getReceiverPhone() {
+        return receiverPhone;
+    }
+
+    public void setReceiverPhone(String receiverPhone) {
+        this.receiverPhone = receiverPhone;
+    }
+
+    public String getReceiverAddress() {
+        return receiverAddress;
+    }
+
+    public void setReceiverAddress(String receiverAddress) {
+        this.receiverAddress = receiverAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getWard() {
+        return ward;
+    }
+
+    public void setWard(String ward) {
+        this.ward = ward;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+}

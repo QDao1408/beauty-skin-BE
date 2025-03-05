c2cf929654ce60505cd0d4440dd39863b5d535b2
Nguyen Quang Dao <nguyenquangdao1408@gmail.com>
2025-03-05 17:12:54 +0700

    crud skin type

---
+++ src/main/java/online/beautyskin/beauty/repository/SkinTypeRepository.java
@@ -0,0 +1,12 @@
+package online.beautyskin.beauty.repository;
+
+import online.beautyskin.beauty.entity.SkinType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SkinTypeRepository extends JpaRepository<SkinType, Long> {
+
+    SkinType findById(long id);
+    List<SkinType> findByIsDeletedFalse();
+}

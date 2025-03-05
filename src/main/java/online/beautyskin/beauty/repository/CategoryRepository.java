0096bfec786646ede1c4c6d2aba621b4391487dc
datct <datctse181971@fpt.edu.vn>
2025-03-05 12:51:24 +0700

    add findProductByCate and findProductByBrand

--- src/main/java/online/beautyskin/beauty/repository/CategoryRepository.java
+++ src/main/java/online/beautyskin/beauty/repository/CategoryRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface CategoryRepository extends CrudRepository<Category, Long> {
     public List<Category> findByIsDeletedFalse();
     public Category findById(long id);
+
+    Category findByName(String categoryName);
 }

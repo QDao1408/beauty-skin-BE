5f4b082e7198863430b7f40e42845ee7fc45876d
datct <datctse181971@fpt.edu.vn>
2025-03-07 02:09:54 +0700

    added order payment

--- src/main/java/online/beautyskin/beauty/service/OrderService.java
+++ src/main/java/online/beautyskin/beauty/service/OrderService.java
@@ -1,10 +1,9 @@
 package online.beautyskin.beauty.service;
 
-import online.beautyskin.beauty.entity.Order;
-import online.beautyskin.beauty.entity.OrderDetail;
-import online.beautyskin.beauty.entity.Product;
+import online.beautyskin.beauty.entity.*;
 import online.beautyskin.beauty.entity.request.OrderDetailsRequest;
 import online.beautyskin.beauty.entity.request.OrderRequest;
+import online.beautyskin.beauty.enums.OrderStatusEnums;
 import online.beautyskin.beauty.repository.OrderRepository;
 import online.beautyskin.beauty.repository.ProductRepository;
 import online.beautyskin.beauty.utils.UserUtils;
@@ -12,9 +11,15 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @Service
 public class OrderService {
@@ -31,12 +36,16 @@ public class OrderService {
     @Autowired
     UserUtils userUtils;
 
-    public Order create(OrderRequest orderRequest) {
+
+    public String create(OrderRequest orderRequest) throws Exception{
         List<OrderDetail> details = new ArrayList<>();
         Order order = modelMapper.map(orderRequest, Order.class);
         order.setOrderDetails(details);
-        order.setUser(userUtils.getCurrentUser());
-//        order.setUserAddress(order.getUser().getAddresses());
+
+        User user = userUtils.getCurrentUser();
+        order.setUser(user);
+//        order.setUserAddress(user.getAddresses().get(0));
+
 
         double totalPrice = 0;
         order.setOrderDate(LocalDateTime.now());
@@ -60,7 +69,87 @@ public class OrderService {
             }
         }
         order.setTotalPrice(totalPrice);
+        Order newOrder = orderRepository.save(order);
+        return createURLPayment(newOrder);
+    }
 
+    public List<Order> getAll() {
+        return orderRepository.findAll();
+    }
+
+    public List<Order> getOrderByUser() {
+        User user = userUtils.getCurrentUser();
+        return orderRepository.findAllByUserId((user.getId()));
+    }
+
+    public String createURLPayment(Order order)throws Exception{
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        LocalDateTime createDate = LocalDateTime.now();
+        String formattedCreateDate = createDate.format(formatter);
+        String orderId = UUID.randomUUID().toString().substring(0, 6);
+
+        String tmnCode = "8FDRRU1S";
+        String secretKey = "2LS6HWZV3VANKGVZW4IFO7L0J8A3406K";
+        String vnpUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+        String returnURL = "http://localhost:8080?orderId="+order.getId();
+
+        String currCode = "VND";
+        Map<String, String> vnpParams = new TreeMap<>();
+        vnpParams.put("vnp_Version", "2.1.0");
+        vnpParams.put("vnp_Command", "pay");
+        vnpParams.put("vnp_TmnCode", tmnCode);
+        vnpParams.put("vnp_Locale", "vn");
+        vnpParams.put("vnp_CurrCode", currCode);
+        vnpParams.put("vnp_TxnRef", orderId);
+        vnpParams.put("vnp_OrderInfo", "Thanh toan cho ma GD: " + orderId);
+        vnpParams.put("vnp_OrderType", "other");
+        vnpParams.put("vnp_Amount", (int) order.getTotalPrice() + "00");
+        vnpParams.put("vnp_ReturnUrl", returnURL);
+        vnpParams.put("vnp_CreateDate", formattedCreateDate);
+        vnpParams.put("vnp_IpAddr", "167.99.74.201");
+
+        StringBuilder signDataBuilder = new StringBuilder();
+        for (Map.Entry<String, String> entry : vnpParams.entrySet()) {
+            signDataBuilder.append(URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8.toString()));
+            signDataBuilder.append("=");
+            signDataBuilder.append(URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8.toString()));
+            signDataBuilder.append("&");
+        }
+        signDataBuilder.deleteCharAt(signDataBuilder.length() - 1); // Remove last '&'
+
+        String signData = signDataBuilder.toString();
+        String signed = generateHMAC(secretKey, signData);
+
+        vnpParams.put("vnp_SecureHash", signed);
+
+        StringBuilder urlBuilder = new StringBuilder(vnpUrl);
+        urlBuilder.append("?");
+        for (Map.Entry<String, String> entry : vnpParams.entrySet()) {
+            urlBuilder.append(URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8.toString()));
+            urlBuilder.append("=");
+            urlBuilder.append(URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8.toString()));
+            urlBuilder.append("&");
+        }
+        urlBuilder.deleteCharAt(urlBuilder.length() - 1); // Remove last '&'
+        return urlBuilder.toString();
+    }
+
+    private String generateHMAC(String secretKey, String signData) throws NoSuchAlgorithmException, InvalidKeyException {
+        Mac hmacSha512 = Mac.getInstance("HmacSHA512");
+        SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "HmacSHA512");
+        hmacSha512.init(keySpec);
+        byte[] hmacBytes = hmacSha512.doFinal(signData.getBytes(StandardCharsets.UTF_8));
+
+        StringBuilder result = new StringBuilder();
+        for (byte b : hmacBytes) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
+    }
+
+    public Order updateStatus(OrderStatusEnums status, long id) {
+        Order order = orderRepository.findOrderById(id);
+        order.setOrderStatus(status);
         return orderRepository.save(order);
     }
 }

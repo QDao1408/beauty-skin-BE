39cdec2713eae7903f4bc98562167b2d794b71a5
Nguyen Quang Dao <nguyenquangdao1408@gmail.com>
2025-03-07 11:56:00 +0700

    crud tag

---
+++ src/main/java/online/beautyskin/beauty/entity/request/TagRequest.java
@@ -0,0 +1,16 @@
+package online.beautyskin.beauty.entity.request;
+
+public class TagRequest {
+    private String name;
+
+    public TagRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

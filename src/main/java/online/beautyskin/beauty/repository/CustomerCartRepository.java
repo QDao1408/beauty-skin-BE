40ecca6d23563a39ae76e6ee9be101527af165bf
datct <datctse181971@fpt.edu.vn>
2025-03-10 04:53:38 +0700

    still wrong

--- src/main/java/online/beautyskin/beauty/repository/CustomerCartRepository.java
+++ src/main/java/online/beautyskin/beauty/repository/CustomerCartRepository.java
@@ -1,10 +1,11 @@
 package online.beautyskin.beauty.repository;
 
 import online.beautyskin.beauty.entity.CustomerCart;
+import online.beautyskin.beauty.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface CustomerCartRepository extends JpaRepository<CustomerCart, Long> {
-    CustomerCart findByUserId(long userId);
+    Optional<CustomerCart> findByUser(User user);
 }

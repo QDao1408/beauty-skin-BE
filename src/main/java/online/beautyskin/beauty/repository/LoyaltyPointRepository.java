3d7fc57108c5c33cde471db9b9affa0b1f06b9ed
Nguyen Quang Dao <nguyenquangdao1408@gmail.com>
2025-03-21 01:23:35 +0700

    loyalty point repository

--- src/main/java/online/beautyskin/beauty/repository/LoyaltyPointRepository.java
+++ src/main/java/online/beautyskin/beauty/repository/LoyaltyPointRepository.java
@@ -3,9 +3,9 @@ package online.beautyskin.beauty.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import online.beautyskin.beauty.entity.LoyaltyPoint;
+import java.util.List;
+
 
 public interface LoyaltyPointRepository extends JpaRepository<LoyaltyPoint, Long>{
-
-    double findAmountLevelById(int i);
     
 }

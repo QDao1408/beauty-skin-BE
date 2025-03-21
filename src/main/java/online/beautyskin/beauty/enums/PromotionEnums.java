5ea3a59ffabdf47a8bbb7f3d9fdef1083f3629d3
Nguyen Quang Dao <nguyenquangdao1408@gmail.com>
2025-03-21 15:27:28 +0700

    relations promo and loyalty

--- src/main/java/online/beautyskin/beauty/enums/PromotionEnums.java
+++ src/main/java/online/beautyskin/beauty/enums/PromotionEnums.java
@@ -1,8 +1,6 @@
 package online.beautyskin.beauty.enums;
 
 public enum PromotionEnums {
-    DISCOUNT_BY_PERCENT,
-    DISCOUNT_BY_AMOUNT,
     APPLY_FOR_ORDER,
     APPLY_FOR_PRODUCT
 }
--- src/main/java/online/beautyskin/beauty/repository/PromotionRepository.java
+++ src/main/java/online/beautyskin/beauty/repository/PromotionRepository.java
@@ -2,16 +2,15 @@ package online.beautyskin.beauty.repository;
 
 import online.beautyskin.beauty.entity.Promotion;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PromotionRepository extends JpaRepository<Promotion, Long> {
 
-    @Query("SELECT p FROM Promotion p WHERE p.startDate <= CURRENT_DATE AND p.endDate >= CURRENT_DATE")
-    List<Promotion> findValidPromotions();
 
     List<Promotion> findAllByIsDeletedFalse();
 
-    Promotion findByIdAndIsDeletedFalse(long id);
+    Promotion findByIdAndIsDeletedFalseAndIsOutDateFalse(long id);
+
+    List<Promotion> findAllByIsOutDateFalseAndIsDeletedFalse();
 }
